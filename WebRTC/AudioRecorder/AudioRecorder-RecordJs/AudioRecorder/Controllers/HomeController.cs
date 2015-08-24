731d851f05d790492b64e816646ef0174f4b6475
Dipak Telangre <dipaktelangre@gmail.com>
2015-08-25 03:06:41 +0530

    Added File Merger to merge chunks of uploaded .WAV files

--- WebRTC/AudioRecorder/AudioRecorder-RecordJs/AudioRecorder/Controllers/HomeController.cs
+++ WebRTC/AudioRecorder/AudioRecorder-RecordJs/AudioRecorder/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Concatenation_Waves;
 using DocBlueList.Web.Managers;
 
 namespace AudioRecorder.Controllers
@@ -38,10 +40,43 @@ namespace AudioRecorder.Controllers
             try
             {
                 var folder = Server.MapPath("~/" + "Files");
-                string fileSuffix = uploadManager.GetTimestamp(DateTime.Now);
+                //string fileSuffix = uploadManager.GetTimestamp(DateTime.Now);
+                var fileName = Request.Form["fileName"];
+                var file = Request.Files["blob"];
+                string filePath = Path.Combine(folder, fileName+".wav");
 
-                Request.SaveAs(Server.MapPath("~/" + "Files/" + fileSuffix + ".wav"), false);
+                string outFilePath = Path.Combine(folder, fileName +"o"+ ".wav");
+                //File not exist, write it 
+
+
+                if (!System.IO.File.Exists(filePath))
+                {
+                    file.SaveAs(filePath);
+                }
+                else
+                {
+                    string tempFile = Path.Combine(folder, fileName+"-temp" + ".wav");
+                    file.SaveAs(tempFile);
+                    string[] filesToMerge = new string[2] {filePath,tempFile};
+
+                    WaveIO wa = new WaveIO();
+                    wa.Merge(filesToMerge, outFilePath);
+
+                    System.IO.File.Delete(filePath);
+                    System.IO.File.Delete(tempFile);
+                    System.IO.File.Move(outFilePath, filePath);
+
+                }
+
+                //var blob = Request.Form["blob"];
+
+                //Request.SaveAs(Server.MapPath("~/" + "Files/" + fileSuffix + ".wav"), false);
                 //uploadManager.Upload(Request.Files, folder, fileSuffix);
+
+                
+
+
+
                 return this.Json(new { Success = true, Status = "Uploaded Successfully" });
             }
             catch (Exception ex)
